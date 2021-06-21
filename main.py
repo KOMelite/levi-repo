56abee4c558ce92c4a3f03fc657bc2b4ec48893d
Chris <fick.christopher@yahoo.com>
2021-06-21 07:49:15 +0200

    Completed overhaul

--- main.py
+++ main.py
@@ -1,7 +1,20 @@
-import levi
+import pandas as pd
 
-from util import prompt_file_path
-from util import determine_year
+from levi import (
+    convert_pdf_to_dataframe,
+    splice_into_series,
+    format_dates,
+    separate_credit_from_debit,
+    to_excel,
+    calculate_vat,
+    calculate_exclusive_vat
+)
+
+from util import (
+    prompt_file_path,
+    determine_year,
+    check_column_merged
+)
 
 if __name__ == '__main__':
 
@@ -10,12 +23,45 @@ if __name__ == '__main__':
 
     year = determine_year()
 
-    df = levi.convert_pdf_to_dataframe(bank_pdf)
+    df = convert_pdf_to_dataframe(bank_pdf)
 
-    # Step four: Structure dataframe for exporting to excel
-    # Note df is now a tuple containing two dataframes... credit and debit
-    df_tuple = levi.structure_dataframe(df, year)
+    df = df.iloc[2:].copy()  # Removes the first 3 lines from dataframe
+    df.dropna(how='any', inplace=True)
+    df.rename({0: "Dates"}, axis=1, inplace=True)
 
-    # Step five: Assign the two dataframes with in df_tuple + Export dataframe to excel
-    df_credit, df_debit = df_tuple
-    levi.to_excel(df_credit, df_debit, excel)
+    expected_characters = 6
+    date_column_is_merged = check_column_merged(column_to_check=df["Dates"],
+                                                expected_characters=expected_characters)
+
+    if date_column_is_merged:
+        df["Dates"] = splice_into_series(series=df["Dates"],
+                                         insert_pos=expected_characters,
+                                         string_to_insert=',')
+
+        df["Dates"] = df["Dates"].str.split(",", expand=True)
+
+    df["Dates"] = format_dates(df["Dates"], year)
+
+    df = df.dropna(how='any')
+
+    df.columns = ["DATE", "DESCRIPTION", "AMOUNT", "Drop_1", "Drop_2"]
+    columns_to_drop = ['Drop_1', 'Drop_2']
+    df.drop(columns_to_drop,
+            axis=1,
+            inplace=True)
+
+    df["AMOUNT"] = [row.replace(",", "") for row in df["AMOUNT"]]
+
+    credit, debit = separate_credit_from_debit(dataframe=df,
+                                               column_name=df["AMOUNT"])
+    credit["AMOUNT"] = [row.replace("Cr", "") for row in credit["AMOUNT"]]
+    credit["AMOUNT"] = pd.to_numeric(credit["AMOUNT"], downcast='float')
+
+    debit["AMOUNT"] = pd.to_numeric(debit["AMOUNT"], downcast="float")
+    debit = debit[debit["AMOUNT"] > 0.00001]
+
+    vat_percent = 15
+    debit[f"VAT ({str(vat_percent)}%)"] = calculate_vat(debit["AMOUNT"], vat_percent)
+    debit["VAT EXCLUSIVE"] = calculate_exclusive_vat(debit["AMOUNT"], vat_percent)
+
+    to_excel(credit, debit, excel)
--- util.py
+++ util.py
@@ -13,19 +13,15 @@ def valid_path(path):
 
 
 def determine_year():
-    print("Are we using the 'current' year for this book: ")
-
     while True:
-        is_current_year = input("y/n : ")
+        current_year = datetime.now().year
+        is_current_year = input(f"Is this the correct year '{current_year}' y/n : ")
         try:
             if is_current_year[0].lower() == "y":
-                year = datetime.now().year
-                year = str(year)
-                return year
+                return current_year
 
             elif is_current_year[0].lower() == "n":
-                year = manually_set_year()
-                return year
+                return manually_set_year()
 
             else:
                 raise ValueError
@@ -39,12 +35,28 @@ def manually_set_year():
     year_character_limit = 4
     try:
         # if year is less than 1676 pandas library freaks out
-        year = int(input("Insert year for this book: "))
-        year = str(year)
-        if len(year) != year_character_limit:
+        year = int(input("Insert correct year for this book: "))
+        str_year = str(year)
+        if len(str_year) != year_character_limit:
             raise ValueError
         return year
 
     except ValueError:
         print(f"Year must contain 4 digits... example(2021/2043/1875 etc)")
         return manually_set_year()
+
+
+def check_column_merged(column_to_check, expected_characters):
+    merged_rows_count = 0
+    merged_rows_threshold = 15
+    is_column_merged = False
+
+    for row in column_to_check:
+        if len(row) > expected_characters:
+            merged_rows_count += 1
+
+            if merged_rows_count >= merged_rows_threshold:
+                is_column_merged = True
+                return is_column_merged
+
+    return is_column_merged
