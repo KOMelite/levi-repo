c1ee1a6be007d3612f46775e14b45124bd8c570e
christopherfick <fick.christopher@yahoo.com>
2022-08-20 10:07:59 +0200

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup
+
+setup(
+    name="levi",
+    author="Christopher Fick",  
+    version="0.1", 
+    install_requires=[
+        "appdirs==1.4.4", 
+        "camelot-py==0.10.1",
+        "certifi==2022.5.18.1",
+        "cffi==1.15.0",
+        "chardet==4.0.0",
+        "charset-normalizer==2.0.12",
+        "click==8.1.3",
+        "cryptography==37.0.2",
+        "et-xmlfile==1.1.0",
+        "ghostscript==0.7",
+        "mypy-extensions==0.4.3",
+        "numpy==1.22.4",
+        "opencv-python==4.5.5.64",
+        "openpyxl==3.0.10",
+        "pandas==1.4.2",
+        "pathspec==0.7.0",
+        "pdfminer.six==20220524",
+        "pdftopng==0.2.3",
+        "pycparser==2.21",
+        "PyPDF2==2.1.0",
+        "python-dateutil==2.8.2",
+        "pytz==2022.1",
+        "six==1.16.0",
+        "tabulate==0.8.9",
+        "typing_extensions==4.2.0",
+        ]
+    )
